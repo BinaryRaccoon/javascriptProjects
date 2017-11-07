b54184895148714e8f4e48b71dfc348a4477afee
KaileyCozart <kaileync@comcast.net>
2017-11-06 20:47:13 -0800

    Still trying to figure out scss loaders

--- Learning Javascript/React Projects/reactApp/components/Hero.jsx
+++ Learning Javascript/React Projects/reactApp/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import CSS from './Hero.scss';
 
 class Hero extends React.Component {
     render() {
         return(
             <div>
-                <h1> Test... </h1>
+                <h1 className={ CSS.test }> Test... </h1>
             </div>
         );
     }
